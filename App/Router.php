1b45000c37c9df574bb024d3b23f4b33897bb462
sivasubramanianj2002 <sivasubramanianj2002@gmail.com>
2024-11-13 14:30:29 +0530

    Update Router.php

--- App/Router.php
+++ App/Router.php
@@ -5,16 +5,25 @@ namespace SDM\App;
 use SDM\App\Controllers\Email;
 use SDM\App\Controllers\Plugin;
 
-class Router {
-    
+class Router
+{
+    /**
+     * Initialize the router by setting up the necessary WordPress hooks and actions.
+     * @return void
+     */
     public function init()
     {
-        $plugin= new Plugin();
+        $plugin = new Plugin();
         $email = new Email();
+
+        // Register the AJAX action for sending emails
         add_action('wp_ajax_send_email', [$email, 'handleEmail']);
+        
+        // Add the plugin admin menu
         add_action('admin_menu', [$plugin, 'addAdminMenu']);
+        
+        // Enqueue plugin-specific scripts and styles in the admin area
         add_action('admin_enqueue_scripts', [$plugin, 'enqueueScripts']);
-        add_action('admin_enqueue_style',[$plugin, 'enqueueScripts']);
+        add_action('admin_enqueue_style', [$plugin, 'enqueueScripts']);
     }
-   
 }
